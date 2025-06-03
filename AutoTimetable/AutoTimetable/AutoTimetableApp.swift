1b77d9af2755b3599759f3df71a9770e34093a40
Fiddich <hiws99@naver.com>
2025-06-03 16:16:30 +0900

    프로젝트 생성

---
+++ AutoTimetable/AutoTimetable/AutoTimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTimetableApp.swift
+//  AutoTimetable
+//
+//  Created by 황인성 on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoTimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoTimetable/AutoTimetable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoTimetable
+//
+//  Created by 황인성 on 6/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
